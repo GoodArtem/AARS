9e4a5df730ca07f2e02dcef61ad780f9d1b7a0d0
Artem Gudkov <gudartem@gmail.com>
2020-05-25 08:40:28 +0300

    Changed loading fonts

--- aars-impl/src/main/java/org/gudartem/aars/impl/pdf/util/PdfUtil.java
+++ aars-impl/src/main/java/org/gudartem/aars/impl/pdf/util/PdfUtil.java
@@ -3,7 +3,11 @@ package org.gudartem.aars.impl.pdf.util;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.pdf.BaseFont;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
 
 public class PdfUtil {
@@ -23,7 +27,9 @@ public class PdfUtil {
     private PdfUtil() {
         BaseFont baseFont;
         try {
-            baseFont = BaseFont.createFont(getClass().getClassLoader().getResource("times.ttf").getFile(), BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            File file = ResourceUtils.getFile("times.ttf");
+            byte[] ttfBytes = Files.readAllBytes(file.toPath());
+            baseFont = BaseFont.createFont( "times.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, false, ttfBytes, null);
             headerFont = new Font(baseFont, 11, Font.BOLD, BaseColor.BLACK);
             commonFont = new Font(baseFont, 8, Font.NORMAL, BaseColor.BLACK);
             smallFont = new Font(baseFont, 6, Font.NORMAL, BaseColor.BLACK);
